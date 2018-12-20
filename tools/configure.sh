3cacad9b2e116d5389c9d0fcf00cab6cf3396d0b
ergotamin <ergotamin.source@gmail.com>
2018-12-20 21:55:51 +0100

    [12/20/18] configure.sh ! [9ec6e7bb]

--- tools/configure.sh
+++ tools/configure.sh
@@ -6,23 +6,23 @@ case ${1} in
     pushd ./src/types &>/dev/zero \
       || exit 1
     npm install \
-      && npm run make:all \
-      && rm -rf ./{.git,node_modules,src,out,index.js,package*} \
-      && mv index.d.ts ../server-interface.d.ts
+      && npm run make:all
     popd &>/dev/zero \
       || exit 1
+    mv ./src/types/{index.d.ts,../lib.d.ts}
+    rm -rf ./src/types
     exit ${?}
     ;;
   postinstall)
-    git clone --single-branch --depth 2 --ipv4 'https://github.com/ergotamin/perl-language-server.git' ./out/server-interface \
+    git clone --single-branch --depth 2 --ipv4 'https://github.com/ergotamin/perl-language-server.git' ./out/lib \
       || exit 1
-    pushd ./out/interface \
+    pushd ./out/lib \
       || exit 1
     npm install \
       && npm run make:node \
       && npm run make:install \
       && npm run make:clean \
-      && rm -rf ./{.git,node_modules,src,out,index.d.ts,package*}
+      && rm -rf ./{.git*,node_modules,src,out,index.d.ts,package*}
     popd &>/dev/zero \
       || exit 1
     exit $?
