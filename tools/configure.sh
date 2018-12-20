dc6cf5b734c431f9ee7d88d5aae986db77a389c0
ergotamin <ergotamin.source@gmail.com>
2018-12-20 20:59:51 +0100

    12/20/18] build system ! [18313477]

---
+++ tools/configure.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+case ${1} in
+  precompile)
+    git clone --single-branch --depth 2 --ipv4 'https://github.com/ergotamin/perl-language-server.git' ./src/types \
+      || exit 1
+    pushd ./src/types &>/dev/zero \
+      || exit 1
+    npm install \
+      && npm run configure \
+      && npm run make:all \
+      && rm -rf ./{.git,node_modules,src,out,index.js,package*} \
+      && mv index.d.ts ../server-interface.d.ts
+    popd &>/dev/zero \
+      || exit 1
+    exit ${?}
+    ;;
+  postinstall)
+    git clone --single-branch --depth 2 --ipv4 'https://github.com/ergotamin/perl-language-server.git' ./out/server-interface \
+      || exit 1
+    pushd ./out/interface \
+      || exit 1
+    npm install \
+      && npm run config \
+      && npm run make:node \
+      && npm run make:install \
+      && npm run make:clean \
+      && rm -rf ./{.git,node_modules,src,out,index.d.ts,package*}
+    popd &>/dev/zero \
+      || exit 1
+    exit $?
+    ;;
+  *)
+    exit $?
+    ;;
+esac
